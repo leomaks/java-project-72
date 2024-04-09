5e78635e48ee57603f09b5c61064613d49cb9ec0
leomaks <s.arkhin@gmail.com>
2024-04-09 11:44:30 +0200

    update 5th step sql

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -1,9 +1,8 @@
 DROP TABLE IF EXISTS urls;
 
 CREATE TABLE urls (
-    id bigint PRIMARY KEY AUTO_INCREMENT,
+    bigint GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255) NOT NULL,
     created_at timestamp
 );
-
 
