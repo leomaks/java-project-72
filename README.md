5e5c1e7eff7e79b44a039703e13da2953f1a5475
leomaks <s.arkhin@gmail.com>
2024-04-09 07:54:25 +0200

    step 4

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 [![Github Actions](https://github.com/leomaks/java-project-72/actions/workflows/github-actions.yml/badge.svg)](https://github.com/leomaks/java-project-72/actions/workflows/github-actions.yml)
 <a href="https://codeclimate.com/github/leomaks/java-project-72/maintainability"><img src="https://api.codeclimate.com/v1/badges/24ebba0ee2dcd4208e7b/maintainability" /></a>
 <a href="https://codeclimate.com/github/leomaks/java-project-72/test_coverage"><img src="https://api.codeclimate.com/v1/badges/24ebba0ee2dcd4208e7b/test_coverage" /></a>
-LINK:
-https://java-project-72-o0te.onrender.com/
+
+LINK: https://java-project-72-o0te.onrender.com/
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,7 +2,9 @@ package hexlet.code;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Collections;
 
+import hexlet.code.dto.BasePage;
 import hexlet.code.repositories.BaseRepository;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinJte;
@@ -11,9 +13,20 @@ import lombok.extern.slf4j.Slf4j;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
+import gg.jte.ContentType;
+import gg.jte.TemplateEngine;
+import gg.jte.resolve.ResourceCodeResolver;
+
 
 @Slf4j
 public class App {
+
+    private static TemplateEngine createTemplateEngine() {
+        ClassLoader classLoader = App.class.getClassLoader();
+        ResourceCodeResolver codeResolver = new ResourceCodeResolver("templates", classLoader);
+        TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
+        return templateEngine;
+    }
 
     static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "7070");
@@ -36,9 +49,19 @@ public class App {
 
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
-            config.fileRenderer(new JavalinJte());
+            config.fileRenderer(new JavalinJte(createTemplateEngine()));
         });
-        app.get("/", ctx -> ctx.result("Hello World"));
+
+
+      //  app.get("/", ctx -> ctx.result("Hello World"));
+//        ctx.render("index.jte", Collections.singletonMap("page", page));
+
+        app.get("/", ctx -> {
+            var page = new BasePage();
+            page.setFlash(ctx.consumeSessionAttribute("flash"));
+            ctx.render("index.jte", Collections.singletonMap("page", page));
+        });
+
 
         log.info("Hello, World error");
 
