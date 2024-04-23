3795edd5be35faea208273e9eec162ec4e48a9da
leomaks <s.arkhin@gmail.com>
2024-04-23 08:55:54 +0200

    add 7 step, tests with mockwebserver

--- app/src/test/java/hexlet/code/TestCheck.java
+++ app/src/test/java/hexlet/code/TestCheck.java
@@ -1,93 +1,76 @@
 package hexlet.code;
 
-//import hexlet.code.model.Url;
-//import hexlet.code.repositories.UrlsRepository;
+import hexlet.code.model.Url;
+import hexlet.code.repositories.UrlsRepository;
 import hexlet.code.util.NamedRoutes;
 import io.javalin.Javalin;
 import io.javalin.testtools.JavalinTest;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.SQLException;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-//import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestCheck {
+
     Javalin app;
+    private static MockWebServer mockServer;
 
     @BeforeEach
-    public final void setUp() throws SQLException, IOException {
+    public final void setUp() throws IOException, SQLException {
         app = App.getApp();
-        MockWebServer server = new MockWebServer();
+    }
+    @BeforeAll
+    public static void startWebServer() throws IOException {
+        mockServer = new MockWebServer();
+        mockServer.start();
+    }
+
+    @AfterAll
+    public static void stopWebServer() throws IOException {
+        mockServer.shutdown();
     }
 
     @Test
-    public void testMainPage() {
+    public void testMain() throws SQLException, IOException, InterruptedException {
+
+        var file = "./src/test/resources/test.html";
+        var body = Files.readString(Paths.get(file));
+
+        MockResponse mockResponse = new MockResponse();
+        mockResponse.setBody(body);
+
+        mockServer.enqueue(mockResponse);
+
+        var url = mockServer.url("/test").toString();
+        var actualUrl = new Url(url);
+        UrlsRepository.save(actualUrl);
+
         JavalinTest.test(app, (server, client) -> {
-            var response = client.get(NamedRoutes.homePath());
+            var id = UrlsRepository.findIdByName(actualUrl.getName());
+            var response = client.post(NamedRoutes.urlCheckPath(id));
+            var responseBody = response.body().string();
+
             assertThat(response.code()).isEqualTo(200);
-            assertTrue((response.body()).string().contains("Бесплатно проверяйте сайты на SEO пригодность"));
+            assertThat(responseBody)
+                    .contains("This is test title")
+                    .contains("This is h1 test")
+                    .contains("Here is description");
+
+            assertThat(responseBody)
+                    .doesNotContain("jkkkkkkkkkkkkkkkj");
+
         });
+
     }
+
 }
-
-/* Создаём инстанс `MockWebServer`. Вызвав на созданном инстансе метод `mockServer.url("/").toString()`
-можно получить адрес сайта, который нужно будет использовать в тестах
-2. Создаём инстанс `MockResponse`, и устанавливаем нужное тело ответа.
-Это и есть та фейковая страница, а точнее её содержимое (html), с которой будет работать наше приложение в тестах
-3. Добавляем инстанс MockResponse в очередь к созданному серверу
-4. Запускаем сервер
-5. После выполнения тестов обязательно нужно остановить сервер.
-Воспользуйтесь аннотациями `@BeforeAll` и `@AfterAll` в тестах
-
-
-public void test() throws Exception {
-  // Create a MockWebServer. These are lean enough that you can create a new
-  // instance for every unit test.
-  MockWebServer server = new MockWebServer();
-
-  // Schedule some responses.
-  server.enqueue(new MockResponse().setBody("hello, world!"));
-  server.enqueue(new MockResponse().setBody("sup, bra?"));
-  server.enqueue(new MockResponse().setBody("yo dog"));
-
-  // Start the server.
-  server.start();
-
-  // Ask the server for its URL. You'll need this to make HTTP requests.
-  HttpUrl baseUrl = server.url("/v1/chat/");
-
-  // Exercise your application code, which should make those HTTP requests.
-  // Responses are returned in the same order that they are enqueued.
-  Chat chat = new Chat(baseUrl);
-
-  chat.loadMore();
-  assertEquals("hello, world!", chat.messages());
-
-  chat.loadMore();
-  chat.loadMore();
-  assertEquals(""
-      + "hello, world!\n"
-      + "sup, bra?\n"
-      + "yo dog", chat.messages());
-
-  // Optional: confirm that your app made the HTTP requests you were expecting.
-  RecordedRequest request1 = server.takeRequest();
-  assertEquals("/v1/chat/messages/", request1.getPath());
-  assertNotNull(request1.getHeader("Authorization"));
-
-  RecordedRequest request2 = server.takeRequest();
-  assertEquals("/v1/chat/messages/2", request2.getPath());
-
-  RecordedRequest request3 = server.takeRequest();
-  assertEquals("/v1/chat/messages/3", request3.getPath());
-
-  // Shut down the server. Instances cannot be reused.
-  server.shutdown();
-}
- */
